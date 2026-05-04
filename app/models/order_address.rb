88494d0540d5d9993e2bc1e702c677784f77ae81
kohaku500 <takotot20002000@gmail.com>
2026-05-04 11:10:23 +0900

    テストファイルの不要なコードを削除し、DB接続設定を修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,21 @@
+# app/models/order_address.rb
+
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token
 
-  # ここにバリデーション（入力チェック）を書く
-
-  def save
-    # ここに保存の命令を書く
+  # ↓ ここが「presence: true（空欄を禁止する）」の設定箇所です
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    # postal_codeの行に「presence: true」が漏れていないか確認し、以下のように書き換えます
+    validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :city
+    validates :addresses
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :token
   end
+
+  # （以下、saveメソッドなどの記述が続きます）
 end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+  end
+
+  describe '商品購入記録の保存' do
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it 'buildingは空でも保存できること' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+    end
+  end
+end
