9e98be739f0a0d3200f5437124d8cfa5b5559c56
kohaku500 <takotot20002000@gmail.com>
2026-05-05 07:07:58 +0900

    エラー時の入力保持機能を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,6 +17,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
   end
