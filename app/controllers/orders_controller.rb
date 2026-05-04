0ae8db2036de7497017e5481e2b6b1cbfaf8a01c
kohaku500 <takotot20002000@gmail.com>
2026-05-04 10:40:24 +0900

    コントローラーに購入機能の保存処理を実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
